0c8cdac81905dd74e1dd0915222caa27e4dfa577
MazenAymann <mazenaymanmo22@gmail.com>
2025-02-05 09:28:20 +0200

    feat(Flutter): Creating Project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:carecomm/utils/routing/app_router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(
+      appRouter: AppRouter(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const MyApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      onGenerateRoute: appRouter.generateRoute,
+    );
+  }
+}
